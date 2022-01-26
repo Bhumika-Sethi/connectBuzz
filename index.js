42b83b77c3b604d5b20703f074c938dd40797611
Bhumika-Sethi <bhumikasethi.1906@gmail.com>
2022-01-27 01:22:21 +0530

    Second commit: Add a controller and router for home

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port = 80;
 
 const app = express();
 
+app.use('/', require('./routes'));
+
+
 app.listen(port, function(err){
     if(err){
         console.log("There is an error in finding the port or we can say running the server", err);
