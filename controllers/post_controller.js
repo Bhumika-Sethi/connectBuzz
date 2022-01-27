95c67548dcbc97e6fbee05b90bf73a5362f1f058
Bhumika-Sethi <bhumikasethi.1906@gmail.com>
2022-01-27 13:59:23 +0530

    Third Commit : Create routes and controllers for  users and posts

---
+++ controllers/post_controller.js
@@ -0,0 +1,3 @@
+module.exports.posts = function(req,res){
+    res.end('<h1>Here we will be posting for our profile</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
-
 router.get('/',homeController.home);
+router.use('/users', require('./users'));
+
+// router.use('/post', require('./post'));
+
+
+// for any futher routes, access from here
+// router.use('/routerName', require('./routerfile'));
+
+console.log('working fine');
+
 module.exports = router;
